4f1d36e4665ddff34b6b8753d313dc5ac8f73ab6
Josh Mather <joshmather@gmail.com>
2017-08-06 12:36:03 -0700

    Modified app.js to handle truffle deploys

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,12 +6,35 @@ import lend_artifacts from '../../build/contracts/lendVault.json'
 
 var LendVault = contract(lend_artifacts);
 
-$(document).ready(getProperties);
+window.fundVault = function() {
+  let fundAmount = $("#fund-amount").val();
+  LendVault.deployed().then(function(contractInstance) {
+    console.log(fundAmount, web3.eth.accounts[0]);
+    contractInstance.fundVault(
+      web3.toWei(fundAmount, 'ether'), {from: web3.eth.accounts[0]}).then(function(v) {
+        //getProperties();
+        LendVault.deployed().then(function(contractInstance) {
+          contractInstance.getProperties.call().then(function(v) {
+            console.log(v);
+            let val = JSON.parse('['+v.toString()+']');
 
+            $("#fund-left").html(web3.fromWei(val[1]));
+            $("#fund-lent").html(web3.fromWei(val[2]));
+            $("#fund-owed").html(web3.fromWei(val[3]));
+            $("#fund-paid").html(web3.fromWei(val[4]));
+            $("#fund-return").html(web3.fromWei(val[5]));
+          });
+        });
+      });
+  });
+}
+
+/*
 function fundVault() {
     let fundAmount = $("#fund-amount").val();
-    LendVault.fundVault({value: web3.toWei(fundAmount, 'ether'), from: web3.eth.accounts[3]}, getProperties);
+    LendVault.fundVault({value: web3.toWei(fundAmount, 'ether'), from: web3.eth.accounts[0]}, getProperties);
 }
+*/
 
 function borrowFunds() {
     let fundsRequested = $("#funds-requested").val();
@@ -24,12 +47,45 @@ function makePayment() {
 }
 
 function getProperties() {
-  let val = JSON.parse('['+LendVault.getProperties.call().toString()+']');
+  LendVault.deployed().then(function(contractInstance) {
+    contractInstance.getProperties.call().then(function(v) {
+      let val = JSON.parse('['+v.toString()+']');
 
-  $("#token-fund").html(web3.fromWei(val[0]));
-  $("#fund-left").html(web3.fromWei(val[1]));
-  $("#fund-lent").html(web3.fromWei(val[2]));
-  $("#fund-owed").html(web3.fromWei(val[3]));
-  $("#fund-paid").html(web3.fromWei(val[4]));
-  $("#fund-return").html(web3.fromWei(val[5]));
+      console.log(val);
+      $("#fund-left").html(web3.fromWei(val[1]));
+      $("#fund-lent").html(web3.fromWei(val[2]));
+      $("#fund-owed").html(web3.fromWei(val[3]));
+      $("#fund-paid").html(web3.fromWei(val[4]));
+      $("#fund-return").html(web3.fromWei(val[5]));
+    });
+  });
 }
+
+//$(document).ready(getProperties);
+$(document).ready(function() {
+  /*
+  if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source like Metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+  */
+    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  //}
+
+  LendVault.setProvider(web3.currentProvider);
+  LendVault.deployed().then(function(contractInstance) {
+    contractInstance.getProperties.call().then(function(v) {
+      let val = JSON.parse('['+v.toString()+']');
+      $("#fund-left").html(web3.fromWei(val[1]));
+      $("#fund-lent").html(web3.fromWei(val[2]));
+      $("#fund-owed").html(web3.fromWei(val[3]));
+      $("#fund-paid").html(web3.fromWei(val[4]));
+      $("#fund-return").html(web3.fromWei(val[5]));
+    });
+  });
+});
+
+
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ var LendVault = artifacts.require("./lendVault.sol");
 
 module.exports = function(deployer) {
   //deployer.deploy(LendVault, ['Rama', 'Nick', 'Jose'], {gas: 500000});
-  deployer.deploy(LendVault, {gas: 400000});
+  deployer.deploy(LendVault, {gas: 500000});
 };
